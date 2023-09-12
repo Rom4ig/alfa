0227fc4145cde09a758b032f8481eab553d7ba8f
Raman <romses2015@gmail.com>
2023-09-12 09:16:31 +0300

    Minor fix

--- tests/bucketWith9Similar.spec.ts
+++ tests/bucketWith9Similar.spec.ts
@@ -10,14 +10,14 @@ test.describe('Корзина', () => {
     await mainPage.navBar.cleanBucket();
 
     await test.step('step_1 Добавить в корзину 9 товаров одного наименования со скидкой', async () => {
-      await mainPage.addItemsByNumberAndDiscount(9, true, 1);
+      await mainPage.addItemsByNumberAndDiscount(9, true, 9);
       expect(await mainPage.navBar.getItemCountsInBucket()).toEqual('9');
     });
 
     await test.step('step_2 Нажать на иконку корзины', async () => {
       await mainPage.navBar.bucketButton.click();
       expect(await mainPage.navBar.bucketPopUp.isOpened()).toBeTruthy();
-      expect(await mainPage.navBar.bucketPopUp.isItemPresentByCount(8)).toBeTruthy();
+      expect(await mainPage.navBar.bucketPopUp.isItemPresentByCount(9)).toBeTruthy();
     });
 
     await test.step('step_3 В окне корзины нажать кнопку перейти в корзину', async () => {
